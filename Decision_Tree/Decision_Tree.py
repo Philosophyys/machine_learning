747badbc003cf9589a6ddc080b69cc873150e263
Shi Xiang Yi <619589959@qq.com>
2017-03-05 22:45:56 +0800

    right

--- Decision_Tree/Decision_Tree.py
+++ Decision_Tree/Decision_Tree.py
@@ -1,11 +1,16 @@
 from entropy import *
 
-def train(test_data):
+sleect_property=[]
+
+def select(test_data):
 	result = {}
 	for i in range(len(test_data[0])-1):
 		data = [[x[i],x[-1]] for x in test_data]
 		result[i]=Select_Threshold(data)
 	x = list(result.items())
-	R = x.sort(key = bin(x[1][1][1]))
-	print x[1][1][1]
-	print result
+	R = sorted(x,key = lambda x:x[1][1])
+	return R[-1]
+
+def train(test_data):
+	for i in range(len(test_data[0])-1):
+		select(data)
