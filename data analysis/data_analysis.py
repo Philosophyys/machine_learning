86bcde1ea784b52352e2fb40b5f3462c9dbc6683
Shi Xiang Yi <619589959@qq.com>
2017-03-14 22:42:43 +0800

    print attributes & types of attributes
    print attributes & types of attributes

--- data analysis/data_analysis.py
+++ data analysis/data_analysis.py
@@ -1,5 +1,21 @@
 import numpy as np
 
+def getdata(filename):
+	input = file(filename,'r')
+	data = input.readlines()
+	xlists = []
+	attributes = []
+	attributes = data.pop(0).split(",")
+	attributes[-1] = attributes[-1].strip()
+	attributes = dict(zip(range(len(attributes)),attributes))
+	print "Output:\nAttributes:\n%s"%attributes.values()
+	for line in data:
+		row =  line.split(",")
+		row[-1] = row[-1].strip()
+		xlists.append(row)
+	data = np.array(xlists)
+	istypes(data)
+
 def istypes(nparry):
 	types = [0]*3
 	(row,col) = nparry.shape
@@ -13,4 +29,4 @@ def istypes(nparry):
 				types[1] +=1
 			else:
 				types[2] +=1
-	print "Output:\nAttributes:\nnumber\t%d\nstrings\t%d\nother\t%d"%(types[0],types[1],types[2])
+	print "number:\t%d\nstrings:\t%d\nother:\t%d"%(types[0],types[1],types[2])
--- data analysis/test.py
+++ data analysis/test.py
@@ -2,14 +2,4 @@ from data_analysis import *
 import numpy as np
 import sys
 
-input = file('F:\dataset.txt','r')
-data = input.readlines()
-xlists = []
-
-for line in data:
-	row =  line.split(",")
-	row[-1] = row[-1].strip()
-	xlists.append(row)
-
-a = np.array(xlists)
-istypes(a)
+getdata('F:\dataset.txt')
